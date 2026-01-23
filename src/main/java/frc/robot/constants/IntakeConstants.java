7d060c8ae8aed506dd173453e8e070a45984040e
anshthedev <anshkool1104@gmail.com>
2026-01-22 18:41:05 -0600

    Adding Constants

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -9,4 +9,21 @@ public class IntakeConstants {
     public static final double REVERSE_SPEED = -0.9; // TODO: Adjust this speed
     public static final double INTAKE_OUT = 0.10; // TODO: Adjust this position
     public static final double INTAKE_IN = 0; // TODO: Adjust this position
+    public static final double TIME_BETWEEN_AGITATION = 0.4;
+
+    public static final double METERS_PER_ROTATION = 0.1595; // π * 0.0508m (circumference of 2-inch sprocket)
+
+
+    public static final double KS = 0.25;
+    public static final double KV = 0.02;
+    public static final double KA = 0.01;
+    public static final double KP = 10;
+    public static final double KI = 0;
+    public static final double KD = 1.0;
+
+    
+    public static final double MOTION_MAGIC_ACCELERATION = 59.54;
+    public static final double CRUISE_VELOCITY = 39.27;
+    public static final double MOTION_MAGIC_JERK = 100.08;
+    
 }
