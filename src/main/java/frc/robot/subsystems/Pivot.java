e6f47e2e7a7681414e11e23f26bed9038439f40d
anshthedev <anshkool1104@gmail.com>
2026-02-03 19:33:06 -0600

    Adding Interface

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.*;
@@ -16,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ShooterConstants;
 
-public class Pivot extends SubsystemBase{
+public class Pivot extends SubsystemBase implements PivotInterface{
 
     private final TalonFX pivotMotor;
 
@@ -35,7 +34,7 @@ public class Pivot extends SubsystemBase{
         pivotMotor.setPosition(0.0);
     }
 
-    private void configureMotors() {
+    public void configureMotors() {
 
         TalonFXConfiguration config = new TalonFXConfiguration();
 
@@ -163,7 +162,7 @@ public class Pivot extends SubsystemBase{
 
         Pose2d hub = (alliance == 'R') ? redHub : blueHub;
 
-                /* ================= Distance to Target ================= */
+        /* ================= Distance to Target ================= */
 
         double dx = hub.getX() - robotPose.getX();
         double dy = hub.getY() - robotPose.getY();
@@ -183,14 +182,6 @@ public class Pivot extends SubsystemBase{
         /* ================= Pitch ================= */
 
         double pitchAngle = Math.atan2(verticalVelocity, horizontalVelocity);
-
-        /* ================= Telemetry ================= */
-
-        // Replace with SmartDashboard / AdvantageKit / custom logger
-        // tkit.RecordOutput("Yaw Angle", params.yawAngle);
-        // tkit.RecordOutput("Pitch Angle", params.pitchAngle);
-        // tkit.RecordOutput("Apex Fraction", apexFraction);
-        // tkit.RecordOutput("Apex Height", apexHeight);
 
         return pitchAngle;
     }
